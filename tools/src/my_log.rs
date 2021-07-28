c33a98dc4efa57545f3b6242fef9bee8d67daf7b
唐健 <tangjian@tangjiandeiMac.local>
2021-07-28 15:45:47 +0800

    优化堆栈错误信息
    优化堆栈错误信息，修改格式和颜色

--- tools/src/my_log.rs
+++ tools/src/my_log.rs
@@ -3,10 +3,18 @@ use simplelog::{CombinedLogger, TermLogger, TerminalMode, WriteLogger};
 use std::fs::File;
 use std::time;
 
+fn setup() {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "1")
+    }
+    color_eyre::install().unwrap();
+}
+
 ///init the log
 ///info_path:the info log path
 ///error_path:the error log path
 pub fn init_log(info_path: &str, error_path: &str) {
+    setup();
     let log_time = time::SystemTime::now();
     let mut config = simplelog::ConfigBuilder::new();
     config.set_time_format_str("%Y-%m-%d %H:%M:%S");
