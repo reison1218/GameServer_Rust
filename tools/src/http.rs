dbfa82967fe598dd1c4e7cf83e5fc635e291027e
reison1218 <tijay830@163.com>
2023-09-14 01:38:43 +0800

    更新http.rs
    get添加uri参数，json参数
    post添加uri参数，uri_params参数
    添加设置head代码
    get和post一并加入路由

--- tools/src/http.rs
+++ tools/src/http.rs
@@ -2,20 +2,28 @@ use std::collections::HashMap;
 
 use super::*;
 use async_std::sync::{Arc as AsyncArc, RwLock as AsyncRwLock};
+use axum::{
+    extract::Query,
+    http::{HeaderMap, HeaderValue},
+    routing::{get, post},
+};
 use serde_json::json;
-
-pub enum HttpMethod {
-    POST,
-    GET,
-}
 
 pub trait HttpServerHandler: Send + Sync {
     fn get_path(&self) -> &str;
-    fn get_method(&self) -> HttpMethod;
-    fn do_post(&mut self, _: serde_json::Value) -> anyhow::Result<serde_json::Value> {
+    fn do_post(
+        &mut self,
+        _uri: String,
+        _uri_params: HashMap<String, String>,
+        _json_params: serde_json::Value,
+    ) -> anyhow::Result<serde_json::Value> {
         Ok(json!(r#"{"statue","success"}"#))
     }
-    fn do_get(&mut self, _: HashMap<&str, &str>) -> anyhow::Result<String> {
+    fn do_get(
+        &mut self,
+        _uri: String,
+        _uri_params: HashMap<String, String>,
+    ) -> anyhow::Result<String> {
         Ok(r#"{"statue","success"}"#.to_string())
     }
 }
@@ -38,50 +46,51 @@ impl Builder {
 
         let handler_lock = TOKIO_RT.block_on(handler.write());
 
-        let http_methond = handler_lock.get_method();
-
         let path = handler_lock.get_path().to_owned();
 
-        std::mem::drop(handler_lock);
+        let handler_lock = handler.clone();
+        let do_post_c =
+            |uri: axum::http::Uri,
+             mut headers: HeaderMap,
+             Query(uri_params): Query<HashMap<String, String>>,
+             axum::Json(json_params): axum::Json<serde_json::Value>| async move {
+                headers.insert(
+                    "Content-Type",
+                    HeaderValue::from_str("text/html;application/json;charset=utf-8").unwrap(),
+                );
+                headers.insert(
+                    "Access-Control-Allow-Origin",
+                    HeaderValue::from_str("*").unwrap(),
+                );
+                let mut handler_lock = handler_lock.write().await;
+                //receive the http request
+                let res = handler_lock.do_post(uri.to_string(), uri_params, json_params);
+                drop(handler_lock);
+                if let Err(e) = res {
+                    error!("{:?}", e);
+                    return (
+                        axum::http::StatusCode::PRECONDITION_FAILED,
+                        axum::Json(json!({"result":"fail"})),
+                    );
+                }
+                (axum::http::StatusCode::CREATED, axum::Json(res.unwrap()))
+            };
 
         let handler_lock = handler.clone();
-        let do_post_c = |axum::Json(params): axum::Json<serde_json::Value>| async move {
+        let do_get_c = |uri: axum::http::Uri,
+                        mut headers: HeaderMap,
+                        Query(uri_params): Query<HashMap<String, String>>| async move {
+            headers.insert(
+                "Content-Type",
+                HeaderValue::from_str("text/html;application/json;charset=utf-8").unwrap(),
+            );
+            headers.insert(
+                "Access-Control-Allow-Origin",
+                HeaderValue::from_str("*").unwrap(),
+            );
             let mut handler_lock = handler_lock.write().await;
             //receive the http request
-            let res = handler_lock.do_post(params);
-            drop(handler_lock);
-            if let Err(e) = res {
-                error!("{:?}", e);
-                return (
-                    axum::http::StatusCode::PRECONDITION_FAILED,
-                    axum::Json(json!({"result":"fail"})),
-                );
-            }
-
-            (axum::http::StatusCode::CREATED, axum::Json(res.unwrap()))
-        };
-
-        let handler_lock = handler.clone();
-        let do_get_c = |uri: axum::http::Uri| async move {
-            let mut handler_lock = handler_lock.write().await;
-            let query = uri.query();
-            if query.is_none() {
-                let res = handler_lock.do_get(HashMap::new());
-                return (axum::http::StatusCode::CREATED, res.unwrap());
-            }
-            let query = query.unwrap();
-            let v: Vec<&str> = query.split("&").collect();
-            let mut map = HashMap::new();
-
-            for _v in v {
-                let vec: Vec<&str> = _v.split("=").collect();
-                map.insert(
-                    vec.get(0).unwrap().to_owned(),
-                    vec.get(1).unwrap().to_owned(),
-                );
-            }
-            //receive the http request
-            let res = handler_lock.do_get(map);
+            let res = handler_lock.do_get(uri.to_string(), uri_params);
             drop(handler_lock);
             if let Err(e) = res {
                 error!("{:?}", e);
@@ -92,11 +101,9 @@ impl Builder {
             }
             (axum::http::StatusCode::CREATED, res.unwrap())
         };
-        let http_methond: axum::routing::MethodRouter = match http_methond {
-            HttpMethod::POST => axum::routing::post(do_post_c),
-            HttpMethod::GET => axum::routing::get(do_get_c),
-        };
-        self.app = self.app.route(path.as_str(), http_methond);
+
+        self.app = self.app.route(path.as_str(), get(do_get_c));
+        self.app = self.app.route(path.as_str(), post(do_post_c));
         self
     }
 
